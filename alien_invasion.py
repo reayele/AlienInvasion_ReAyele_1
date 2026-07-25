87db7b8cbf079b1aaf1ad97c844342c9cb582094
Rediet Ayele <reayele@gmail.com>
2026-07-25 19:38:43 -0400

    Create initial game window

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,28 @@
 import sys
 import pygame
 
-if __name__ == '__main__':
-    pass
+
+class AlienInvasion:
+    def __init__(self):
+        pygame.init()
+
+        self.clock = pygame.time.Clock()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Galaxy Defender - Track 2")
+
+    def run_game(self):
+        while True:
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            pygame.display.flip()
+            self.clock.tick(60)
+
+
+if __name__ == "__main__":
+    ai = AlienInvasion()
+    ai.run_game()
