1af46eb83f7df1222d1adc9354cf9e7e108c8602
Rediet Ayele <reayele@gmail.com>
2026-07-25 20:28:09 -0400

    Added the game settings class

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,14 +1,16 @@
 import sys
 import pygame
+from settings import Settings
 
 
 class AlienInvasion:
     def __init__(self):
         pygame.init()
-
         self.clock = pygame.time.Clock()
-
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
         pygame.display.set_caption("Galaxy Defender - Track 2")
 
     def run_game(self):
---
+++ settings.py
@@ -0,0 +1,6 @@
+class Settings:
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.resolution = (self.screen_width, self.screen_height)
+        self.bg_color = (230, 230, 230)
