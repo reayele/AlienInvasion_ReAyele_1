c4f57f133be841608211694e044e591a29c4c934
Rediet Ayele <reayele@gmail.com>
2026-07-26 02:05:22 -0400

    Added quit key and the keybaord controls

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,28 +21,32 @@ class AlienInvasion:
             self._update_screen()
             self.clock.tick(60)
             self.ship.update()
-
     def _check_events(self):
         for event in pygame.event.get():
-        
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            pygame.quit()
+            sys.exit()
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
         pygame.display.flip()
-
 if __name__ == "__main__":
     ai = AlienInvasion()
     ai.run_game()
