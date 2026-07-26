d3a1662eb4134243bd919da576839fbabb588b84
Rediet Ayele <reayele@gmail.com>
2026-07-26 01:30:03 -0400

    Made ship movement faster

--- ship.py
+++ ship.py
@@ -1,10 +1,12 @@
 from pathlib import Path
-import pygame 
-class Ship:
+import pygame
 
+class Ship:
     def __init__(self, ai_game):
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
+
         project_folder = Path(__file__).resolve().parent
         image_path = (
             project_folder
@@ -15,12 +17,18 @@ class Ship:
         self.image = pygame.image.load(image_path).convert_alpha()
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
+
         self.moving_right = False
         self.moving_left = False
+
     def update(self):
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
+
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+        self.rect.x = self.x
+
     def blitme(self):
         self.screen.blit(self.image, self.rect)
