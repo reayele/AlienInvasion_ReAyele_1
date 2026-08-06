9ff7d70056a2b6e0005bb48e33c7bfe8be42f13d
Rediet Ayele <reayele@gmail.com>
2026-08-05 21:06:21 -0400

    Created a full meteor fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -79,35 +79,30 @@ class AlienInvasion:
         """Creats a new laser"""
         new_bullet = Bullet(self)
         self.bullets.add(new_bullet)
-    
     def _create_fleet(self):
         alien = Alien(self)
-        alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         current_x = alien_width
+        current_y = alien_height
 
-        while current_x < (
+        while current_y < (
+            self.settings.screen_height - 3 * alien_height
+        ):
+            while current_x < (
             self.settings.screen_width - 2 * alien_width
-        ):
-            new_alien = Alien(self)
-            new_alien.x = current_x
-            new_alien.rect.x = current_x
-            self.aliens.add(new_alien)
-            current_x += 2 * alien_width
-    def _create_fleet(self):
-        alien = Alien(self)
-        alien_width = alien.rect.width 
-        current_x = alien_width 
-        while current_x < (
-            self.settings.screen_width - 2 * alien_width 
-        ):
-            self._create_alien(current_x)
-            current_x += 2 * alien_width
-    def _create_alien(self, x_position):
+            ):
+                self._create_alien(current_x, current_y)
+                current_x += 2 * alien_width
+            current_x = alien_width
+            current_y += 2 * alien_height
+    def _create_alien(self, x_position, y_position):
+    
         new_alien = Alien(self)
         new_alien.x = x_position
         new_alien.rect.x = x_position
+        new_alien.rect.y = y_position
         self.aliens.add(new_alien)
+
     def _update_screen(self):
         """Draws the games objects on the screen"""
         self.screen.blit(self.background, (0, 0))
