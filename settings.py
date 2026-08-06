7a6463ba2446ece5e1b3706f6fe112b3138899ef
Rediet Ayele <reayele@gmail.com>
2026-08-05 22:35:34 -0400

    Fixing some issues and making adjustments

--- settings.py
+++ settings.py
@@ -4,7 +4,9 @@ class Settings:
         """Sets the game settings"""
         self.screen_width = 1200
         self.screen_height = 800
-        self.alien_speed = 1.0
+        self.alien_speed = 3.0
+        self.fleet_drop_speed = 10 
+        self.fleet_direction = 1
         self.bg_color = (230, 230, 230)
         self.ship_speed = 1.6
         self.bullet_speed = 2.5 
