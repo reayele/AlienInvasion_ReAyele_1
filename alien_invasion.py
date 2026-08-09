a7bd149527b0364f28bc2e99bbdf9dae43d7e092
Rediet Ayele <reayele@gmail.com>
2026-08-09 19:45:20 -0400

    Game started when the button is clicked

--- alien_invasion.py
+++ alien_invasion.py
@@ -81,6 +81,13 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
+    def _check_play_button(self, mouse_pos):
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.game_active: 
+            self.game_active = True
 
     def _check_keydown_events(self, event):
         """Checks when a key is pressed"""
