34a94a46a9958529b89a8e505a0108c6f519999c
Rediet Ayele <reayele@gmail.com>
2026-08-09 17:55:57 -0400

    Created the reusable button class

---
+++ button.py
@@ -0,0 +1,29 @@
+"""
+Author: Rediet Ayele
+Purpose: Creates a reusable play button
+date: August 9, 2026
+"""
+import pygame.font
+class Button: 
+    def __init___(self, ai_game, msg):
+        self.screen = ai_game.screen 
+        self.screen_rect = self.screen.get_rect()
+        self.width, self.height = 200, 50 
+        self.button_color = (0 ,135, 0)
+        self.text_color = (255, 255, 255)
+        self.font = pygame.font.SysFont(None, 48)
+        self.rect = pygame.Rect(0, 0, self.width, self.height)
+        self._prep_msg(msg)
+    def _prep_msg(self, msg):
+        self.msg_image = self.font.render(
+            msg,
+            True,
+            self.text_color, 
+            self.button_color,
+        )
+
+        self.msg_image_rect = self.msg_image.get_rect()
+        self.msg_image_rect.center = self.rect.center
+    def draw_button(self):
+        self.screen.fill(self.button_color, self.rect)
+        self.screen.blit(self.msg_image, self.msg_image_rect)
